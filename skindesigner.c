dd253591501ffd4c574687db960e2647dc6e1573
louis <louis.braun@gmx.de>
2016-04-17 09:30:31 +0200

    Version 1.0.0

--- skindesigner.c
+++ skindesigner.c
@@ -19,7 +19,7 @@
 #endif
 
 
-static const char *VERSION        = "0.9.6";
+static const char *VERSION        = "1.0.0";
 static const char *DESCRIPTION    = trNOOP("Skin Designer");
 
 class cPluginSkinDesigner : public cPlugin, public skindesignerapi::SkindesignerAPI {
