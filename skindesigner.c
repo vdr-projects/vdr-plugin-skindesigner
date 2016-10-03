c8d54dbe256773e88e714e1cdb7160ca9dc2f927
louis <louis.braun@gmx.de>
2016-10-03 15:49:57 +0200

    Version 1.2.3

--- skindesigner.c
+++ skindesigner.c
@@ -20,7 +20,7 @@
 #endif
 
 
-static const char *VERSION        = "1.2.2";
+static const char *VERSION        = "1.2.3";
 static const char *DESCRIPTION    = trNOOP("Skin Designer");
 
 class cPluginSkinDesigner : public cPlugin, public skindesignerapi::SkindesignerAPI {
